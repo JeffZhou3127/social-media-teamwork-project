3bba30b69e42450cd9763e6d233b499746ebdf76
Stephen <stephen20120401@gmail.com>
2025-10-03 22:29:57 -0700

    header

--- src/app/AppLayout.jsx
+++ src/app/AppLayout.jsx
@@ -2,6 +2,16 @@ import React from "react";
 import { Outlet } from "react-router-dom";
 import styles from "./AppLayout.module.css";
 
-const AppLayout = () => <Outlet />;
+const AppLayout = () => {
+    return (
+        <>
+            <header className={styles.header}>
+              <h1 className={styles.h1}>Hello, World!!!</h1>
+            </header>
+            <main className={styles.main}><Outlet /></main>
+        </>
+    );
+};
+
 
 export default AppLayout;
