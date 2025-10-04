c4ab3219ecbfe683c2c1cf902802f0c04ebf7263
Stephen <stephen20120401@gmail.com>
2025-10-03 21:08:34 -0700

    displayes post data

--- src/features/posts/Post.jsx
+++ src/features/posts/Post.jsx
@@ -3,15 +3,19 @@ import React from "react";
 const Post = ({ postTitle, postImage, postDescription, author }) => {
   // Render something here with the information
   // If author is an empty string, set it to unknown
-  return <p>
-      Title: {postTitle}
-      <br />
-      Image Url: {postImage}
-      <br />
-      Description: {postDescription}
-      <br />
-      Author: {author}
-    </p>; // Replace this with actual content
+  return (
+    <section>
+      <img height="100" src={postImage}></img>
+      
+      <p>Posted by {author ? author : "unknown"}</p>
+      <br></br>
+      <article>
+        <h2>{postTitle}</h2>
+        <p>{postDescription}</p>
+      </article>
+      <br></br><br></br>
+    </section>
+  );
 };
-
+console.log(Post);
 export default Post;
