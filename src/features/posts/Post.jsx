e8bae5ddcf1d28c5c994602fb14f5cf0fc9df88c
Jeff Zhou <zhouj0805@gmail.com>
2025-10-03 21:19:45 -0700

    Fix Post.jsx

--- src/features/posts/Post.jsx
+++ src/features/posts/Post.jsx
@@ -1,21 +1,16 @@
 import React from "react";
 
 const Post = ({ postTitle, postImage, postDescription, author }) => {
-  // Render something here with the information
-  // If author is an empty string, set it to unknown
   return (
     <section>
-      <img height="100" src={postImage}></img>
-      
-      <p>Posted by {author ? author : "unknown"}</p>
-      <br></br>
+      <img src={postImage} alt={postDescription} />
       <article>
         <h2>{postTitle}</h2>
+        <p>Posted by {author ? author : "unknown"}</p>
         <p>{postDescription}</p>
       </article>
-      <br></br><br></br>
     </section>
   );
 };
-console.log(Post);
+
 export default Post;
